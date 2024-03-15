31b22607007275a6e754874f95318db92c3da981
Damian Amalraj. <damian.amalraj@hotmail.com>
2024-03-15 18:40:34 +0100

    Add ESLint rule and update layout and page components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Header from '@/components/Header';
+import Header from '@/components/header';
+import { metadataInfo } from '@/lib/data';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Daniel | Full-Stack Developer',
-  description:
-    'I am a full-stack developer with 2 years of experience. I specialize in React, Next.js, TypeScript, .NET, SQL and NoSQL databases. I am open to full-time opportunities.',
+  title: metadataInfo.title,
+  description: metadataInfo.description,
 };
 
 export default function RootLayout({
@@ -17,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950`}>
-        <div className="bg-red-400 absolute top-0 left-[40%] h-[32rem] w-[32rem] rounded-full blur-[32rem] sm:w-[40rem]"></div>
-        <div className="bg-blue-400 absolute top-0 right-[40%] h-[32rem] w-[32rem] rounded-full blur-[32rem] sm:w-[40rem]"></div>
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 pt-28 sm:pt-36`}
+      >
+        <div className="bg-red-400 absolute top-0 right-[15%] h-[40rem] w-[32rem] rounded-full blur-[32rem] sm:w-[40rem] "></div>
+        <div className="bg-blue-400 absolute top-0 right-[40%] h-[40rem] w-[32rem] rounded-full blur-[32rem] sm:w-[40rem]"></div>
 
         <Header />
         {children}
